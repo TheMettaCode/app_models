5541e4b2bc23f826a3661520b7956b39d4816901
MettaCode <mettacode@gmail.com>
2024-04-30 21:31:39 -0500

    updated

--- lib/payments/stripe/stripe_helper.dart
+++ lib/payments/stripe/stripe_helper.dart
@@ -676,9 +676,9 @@ class StripeHelper {
     //   HttpHeaders.authorizationHeader: "Bearer $stripeSecretApiKey",
     // };
 
-    Map<String, String> data = {"active": "true"};
+    Map<String, dynamic> data = {"active": "true"};
     if (productIds != null && productIds.isNotEmpty) {
-      data.addAll({"ids[]": productIds.toString()});
+      data.addAll({"ids[]": productIds});
     }
 
     try {
